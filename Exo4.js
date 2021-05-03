cb11947d53695a9bfc6a976d01a2716fc487b068
GuillaumeDavid23 <guillaume.david744@orange.fr>
2021-05-03 11:23:19 +0200

    Exo4

---
+++ Exo4.js
@@ -0,0 +1,18 @@
+
+let note = parseInt(prompt("Valeur :"));
+
+if (note>=0 & note<=3 ){
+    alert("Nul")
+}
+else if(note>=4 & note<6){
+    alert("Moyen")
+}
+else if(note>=6 & note<8){
+    alert("Assez Bien")
+}
+else if(note>=8 & note<=9){
+    alert("Bien")
+}
+else{
+    alert("Excellent")
+}
